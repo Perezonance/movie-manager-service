9dd54d63d7585928b8240968b120781d22112360
Manuel Alex Perez <manuel.parez@toyota.com>
2020-06-10 18:29:26 -0400

    setting up main package

--- pkg/main.go
+++ pkg/main.go
@@ -1,1 +1,26 @@
-package pkg
+package main
+
+import(
+	"fmt"
+	"net/http"
+)
+
+const (
+	root = "/api/v1"
+	port = "8080"
+	addr = "0.0.0.0"
+	fullAddr = addr + ":" + port
+)
+
+func main() {
+	fmt.Println(start())
+}
+
+func start() error{
+	http.HandleFunc(root + "/movie", movieHandler)
+	return http.ListenAndServe(fullAddr, nil)
+}
+
+func movieHandler(w http.ResponseWriter, r *http.Request) {
+
+}
